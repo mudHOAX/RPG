352a0b65e9caeb8d64371a285a0bbae22546b92e
Ian Munro <ijm1990@gmail.com>
2015-08-03 16:23:22 +0100

    Adding in the remaining armours.

--- Assets/Scripts/Character/Zidane.cs
+++ Assets/Scripts/Character/Zidane.cs
@@ -17,13 +17,18 @@ public class Zidane : BaseCharacter
             Weapon = ItemManager.Weapons.GetById(1),
             Head = ItemManager.Armours.GetById(14)
         };
-        
+
         Debug.Log("------------------------------");
         Debug.Log("Level: " + Level);
         Debug.Log("Speed: " + Stats.Speed);
         Debug.Log("Strength: " + Stats.Strength);
         Debug.Log("Magic: " + Stats.Magic);
         Debug.Log("Spirit: " + Stats.Spirit);
+        Debug.Log("Attack: " + Stats.Attack);
+        Debug.Log("Defence: " + Stats.Defence);
+        Debug.Log("Evade: " + Stats.Evade);
+        Debug.Log("MagicDefence: " + Stats.MagicDefence);
+        Debug.Log("MagicEvade: " + Stats.MagicEvade);
         Debug.Log("------------------------------");
     }
 }
